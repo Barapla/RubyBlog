cafb66a425ca94f9ff2e4b28678ae5d26b78bc7e
Barapla <araujobryan333@gmail.com>
2021-09-28 20:11:51 -0700

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'tags/index'
+  get 'tags/show'
+  get 'tags/create'
+  get 'tags/update'
+  get 'tags/destroy'
+  get 'tags/random'
+  resources :categories
   root 'pages#home'
   get 'about',  to: 'pages#about'
 end
